21b2ccafc4c8f2ec44a76d8c5745142d6935cd6d
Nikita Korobkov <nikkorobk@gmail.com>
2019-12-05 15:03:18 -0500

    Styling of form

--- interview/form.py
+++ interview/form.py
@@ -6,3 +6,8 @@ class IssueForm(forms.Form):
     panel = forms.ModelChoiceField(Panel.objects.using('interview_db').all(), label='panel')
     site = forms.ModelChoiceField(Site.objects.using('interview_db').all(), label='site')
     comment = forms.CharField(label='comment', max_length=1024, widget=forms.Textarea(), required=False)
+
+    def __init__(self):
+        super(IssueForm, self).__init__()
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
